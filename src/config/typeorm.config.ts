382ac517315ed73cd6800818881ddee55d1358d5
christianfus <christianfus@gmail.com>
2019-12-19 19:39:58 +0100

    test heroku 4

--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -1,15 +1,19 @@
-import { TypeOrmModuleOptions } from "@nestjs/typeorm";
-import * as config from "config";
+import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+import * as config from 'config';
+import * as PostgressConnectionStringParser from 'pg-connection-string';
 
+const connectionOptions = PostgressConnectionStringParser.parse(
+  process.env.DATABASE_URL,
+);
 const dbConfig = config.get('db');
 
 export const typeOrmConfig: TypeOrmModuleOptions = {
-    type: dbConfig.type,
-    host: process.env.DATABASE_URL || dbConfig.host,
-    port: dbConfig.port,
-    username: dbConfig.username,
-    password: dbConfig.password,
-    database: dbConfig.database,
-    entities: [__dirname + '/../**/*.entity.{js,ts}'],
-    synchronize: dbConfig.synchronize,
-}
+  type: dbConfig.type,
+  host: connectionOptions.host || dbConfig.host,
+  port: Number.parseInt(connectionOptions.port),
+  username: connectionOptions.user,
+  password: connectionOptions.password,
+  database: connectionOptions.database,
+  entities: [__dirname + '/../**/*.entity.{js,ts}'],
+  synchronize: dbConfig.synchronize,
+};
